07829e226274967fe604e0fd04deeb20c2fc1e4f
projetoreciclando <64789106+projetoreciclando@users.noreply.github.com>
2020-05-11 09:00:02 -0300

    Update cadastro.page.ts

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Cadastrocliente } from '../cadastrocliente';
+import { CadastroclienteService } from '../cadastrocliente.service';
+import { CadastroclienteDadosService } from '../cadastrocliente-dados.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
+
+
 export class CadastroPage implements OnInit {
+  cadastrocliente: Cadastrocliente;
+  key: string= '';
 
-  constructor() { }
+  constructor(private cadastroclienteService: CadastroclienteService, private cadastroclienteDataService: CadastroclienteDadosService, private router: Router) { 
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.cadastrocliente = new Cadastrocliente();
+    this.cadastroclienteDataService.currentCadastrocliente.subscribe(data =>{
+      if (data.cadastrocliente && data.key){
+        this.cadastrocliente = new Cadastrocliente();
+        this.cadastrocliente.nome = data.cadastrocliente.nome;
+        this.cadastrocliente.cpf = data.cadastrocliente.cpf;
+        this.cadastrocliente.telefone = data.cadastrocliente.telefone;
+        this.cadastrocliente.endereco = data.cadastrocliente.endereco;
+        this.cadastrocliente.complemento = data.cadastrocliente.complemento;
+        this.cadastrocliente.estado = data.cadastrocliente.estado;
+        this.cadastrocliente.cep = data.cadastrocliente.cep;
+        this.key = data.key;
+      }
+    });
+  }
+
+ // Não sei oq por aqui// 
+  onSubmit(){
+    this.cadastroclienteService.insert(this.cadastrocliente);
+    this.router.navigate(['/cadastro']);
+    this.cadastrocliente = new Cadastrocliente();
+  }  
+  
 }
