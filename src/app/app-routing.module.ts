c6ac1940d4ccaff6e1b8725b557652472bac96df
projetoreciclando <projetoreciclandosenac@gmail.com>
2020-05-13 08:15:11 -0300

    Guards Desabilidatos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,13 +15,12 @@ const routes: Routes = [
   {
     path: "home",
     loadChildren: () =>
-    import('./home/home.module').then(m => m.HomePageModule),
-    canActivate: [AuthGuard]
+    import('./home/home.module').then(m => m.HomePageModule)
+
   },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
-    canActivate: [HomeGuard]
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
 
   }, 
   {
